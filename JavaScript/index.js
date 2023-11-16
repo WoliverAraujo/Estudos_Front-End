33479021db728afa1c86cd539fb116700c3535d4
Woliver <woliveraraujo@hotmail.com>
2023-11-16 18:59:37 -0300

    Update

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+const nome = 'Woliver';
+const sobrenome = 'Araujo';
+const idade = 34;
+const peso = 84;
+const altura = 1.75;
+let imc = peso / (altura*altura)
+let anoNascimento= 2023 - idade;
+
+console.log (nome, sobrenome, 'tem' ,idade, 'pesa' ,peso,'kg')
+console.log('Tem' , altura, 'e seu IMC é de',imc,)
+console.log (nome, 'nasceu em', anoNascimento)
