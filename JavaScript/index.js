e375c6c3b4fb11094f68d7f68187c4f3dbf4fd39
Woliver <woliveraraujo@hotmail.com>
2023-11-22 11:40:45 -0300

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,8 @@
-alert('Alerta')
+let num1 = prompt("Digite um numero");
+let num2 = prompt("Digite outro numero");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+const resultado = num1 + num2
+alert ("O resultado foi"+ resultado)
