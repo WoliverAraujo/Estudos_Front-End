0ebb7302a553b8787e74eae756ffe5f9def8590c
Woliver <woliveraraujo@hotmail.com>
2023-12-07 20:36:04 -0300

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,6 @@
-const numero = prompt('Digite um numero');
-const numeroTitulo =  document.getElementeById('numero-titulo');
-const nomeTitulo =  document.getElementeById('texto');
+const numero = Number (prompt('Digite um numero'));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
 
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Seu número - 2 é ${numero + 2}</p>`;
