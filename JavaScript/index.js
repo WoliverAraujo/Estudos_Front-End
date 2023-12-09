63b890882b15c325420f5002da8a8a217e15c0b5
Woliver <woliveraraujo@hotmail.com>
2023-12-09 12:16:18 -0300

    Exercício Match - funcionando

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,16 +1,18 @@
 const numero = Number (prompt('Digite um numero'));
 const numeroTitulo = document.getElementById('numero-titulo');
-const raizQuadrada = document.getElementById('Raiz quadrada');
-const inteiro = document.getElementById('Inteiro');
-const nan = document.getElementById('NaN');
-const baixo = document.getElementById('Baixo');
-const cima = document.getElementById('Cima');
-const duas = document.getElementById('Duas');
+const raizQuadrada = document.getElementById('raiz-quadrada');
+const inteiro = document.getElementById('inteiro');
+const nan = document.getElementById('nan');
+const baixo = document.getElementById('baixo');
+const cima = document.getElementById('cima');
+const duas = document.getElementById('duas');
+
+
 
 numeroTitulo.innerHTML = numero;
 raizQuadrada.innerHTML = `<p>A raiz quadrada de ${numero} é ${Math.sqrt(numero)}</p>`;
-inteiro.innerHTML = `<p>${numero}é inteiro: ${Number.isInteger}</p>`;
-nan.innerHTML = `<p>${numero}É NaN: ${Number.isInteger}</p>`;
-baixo.innerHTML = `<p>${numero}Arredondando para baixo: ${Math.round(numero)}</p>`;
-cima.innerHTML = `<p>${numero}Arredondando para cima ${Math.ceil(numero)}</p>`;
-duas.innerHTML = `<p>${numero} Com duas casas decimais: ${Number.isInteger}</p>`;
+inteiro.innerHTML = `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+nan.innerHTML = `<p> É NaN: ${Number.isNaN(numero)}</p>`;
+baixo.innerHTML = `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+cima.innerHTML = `<p>Arredondando para cima ${Math.ceil(numero)}</p>`;
+duas.innerHTML = `<p>Com duas casas decimais: ${parseFloat(numero.toFixed(2))}</p>`;
