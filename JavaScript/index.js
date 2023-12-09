0b27f5065d5087b9d773aeb90ba9af751af8c46e
Woliver <woliveraraujo@hotmail.com>
2023-12-09 12:42:15 -0300

    Exercício Match - funcionando
    Correção do professor não precisou mexer no HTML

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,18 +1,14 @@
 const numero = Number (prompt('Digite um numero'));
 const numeroTitulo = document.getElementById('numero-titulo');
-const raizQuadrada = document.getElementById('raiz-quadrada');
-const inteiro = document.getElementById('inteiro');
-const nan = document.getElementById('nan');
-const baixo = document.getElementById('baixo');
-const cima = document.getElementById('cima');
-const duas = document.getElementById('duas');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML=numero;
 
 
-
-numeroTitulo.innerHTML = numero;
-raizQuadrada.innerHTML = `<p>A raiz quadrada de ${numero} é ${Math.sqrt(numero)}</p>`;
-inteiro.innerHTML = `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
-nan.innerHTML = `<p> É NaN: ${Number.isNaN(numero)}</p>`;
-baixo.innerHTML = `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
-cima.innerHTML = `<p>Arredondando para cima ${Math.ceil(numero)}</p>`;
-duas.innerHTML = `<p>Com duas casas decimais: ${parseFloat(numero.toFixed(2))}</p>`;
+texto.innerHTML = '';
+texto.innerHTML += `<p>A raiz quadrada de ${numero} é ${Math.sqrt(numero)}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p> É NaN: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${parseFloat(numero.toFixed(2))}</p>`;
