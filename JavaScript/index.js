e7a2dc663e6b96fedcc7bf9c17256159111e28b3
Woliver <woliveraraujo@hotmail.com>
2023-12-09 11:49:24 -0300

    Update
    Ainda não está funcionando

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,16 @@
 const numero = Number (prompt('Digite um numero'));
 const numeroTitulo = document.getElementById('numero-titulo');
-const texto = document.getElementById('texto');
+const raizQuadrada = document.getElementById('Raiz quadrada');
+const inteiro = document.getElementById('Inteiro');
+const nan = document.getElementById('NaN');
+const baixo = document.getElementById('Baixo');
+const cima = document.getElementById('Cima');
+const duas = document.getElementById('Duas');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Seu número - 2 é ${numero + 2}</p>`;
+raizQuadrada.innerHTML = `<p>A raiz quadrada de ${numero} é ${Math.sqrt(numero)}</p>`;
+inteiro.innerHTML = `<p>${numero}é inteiro: ${Number.isInteger}</p>`;
+nan.innerHTML = `<p>${numero}É NaN: ${Number.isInteger}</p>`;
+baixo.innerHTML = `<p>${numero}Arredondando para baixo: ${Math.round(numero)}</p>`;
+cima.innerHTML = `<p>${numero}Arredondando para cima ${Math.ceil(numero)}</p>`;
+duas.innerHTML = `<p>${numero} Com duas casas decimais: ${Number.isInteger}</p>`;
